47ca72da45ec40cf455c17515aa1624c79de9f64
genturarif <gentur1608@gmail.com>
2021-08-11 11:21:05 +0700

    update javascript

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const display1El = document.querySelector(".display-1");
 const display2El = document.querySelector(".display-2");
 const tempResultEl = document.querySelector(".temp-result");
-const numberEl = document.querySelectorAll(".number");
+const numbersEl = document.querySelectorAll(".number");
 const operationEl = document.querySelectorAll(".operation");
 const equalEl = document.querySelector(".equal");
-const clearEl = document.querySelector(".all-clear");
-const clearLastEl = document.querySelector("last-entity-clear");
+const clearAllEl = document.querySelector(".all-clear");
+const clearLastEl = document.querySelector(".last-entity-clear");
 
 let dis1Num = "";
 let dis2Num = "";
 let result = null;
 let lastOperation = "";
 let haveDot = false;
+
+// Numbers and Dot(.) display 2
+numbersEl.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    if (e.target.innerText === "." && !haveDot) {
+      haveDot = true;
+    } else if (e.target.innerText === "." && haveDot) {
+      return;
+    }
+    dis2Num += e.target.innerText;
+    display2El.innerText = dis2Num;
+  });
+});
